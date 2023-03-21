ed5068c5523c196b3e5e218affe37fb4c80117db
gmaralan <gmartinialan@gmail.com>
2023-03-21 07:04:45 -0400

    feat: products router skeleton

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const { productsController } = require('../controllers');
 
 const productsRouter = express.Router();
+
+productsRouter.get('/', productsController.findAll);
+productsRouter.get('/:id', productsController.findById);
+productsRouter.get('/search', productsController.findByQuery);
+productsRouter.post('/', productsController.create);
+productsRouter.put('/:id', productsController.update);
+productsRouter.delete('/:id', productsController.delete);
 
 
 module.exports = productsRouter;
