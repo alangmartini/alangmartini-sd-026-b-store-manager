d2a1922974adb9fcfb221bb2995f75087dcce77e
gmaralan <gmartinialan@gmail.com>
2023-03-21 06:56:16 -0400

    created app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/products', productsRouter);
+
+app.use('/sales', salesRoutes);
+
+module.exports = app;
