1f06bb3bd15d942e96320b3853b1dd7a11398af2
gmaralan <gmartinialan@gmail.com>
2023-03-21 07:19:18 -0400

    feat: products service skeleton

---
+++ src/services/products.service.js
@@ -0,0 +1,95 @@
+const productsModel = require('../models');
+
+const findAll = async () => {
+  const result = productsModel.findAll(id);
+
+  if (!result) {
+    return {
+      type: ERRORS_TYPE.NOT_FOUND,
+      message: ERRORS_MESSAGE.NOT_FOUND,
+      error: new Error('Nenhum resultado retornado'),
+    }
+  }
+
+  return result;
+};
+
+const findById = async () => {
+  const result = productsModel.findById(id);
+
+  if (!result) {
+    return {
+      type: ERRORS_TYPE.NOT_FOUND,
+      message: ERRORS_MESSAGE.NOT_FOUND,
+      error: new Error('Nenhum resultado retornado'),
+    }
+  }
+
+  return result;
+};
+
+const findBy = async () => {
+  const result = productsModel.findBy(id);
+
+  if (!result) {
+    return {
+      type: ERRORS_TYPE.NOT_FOUND,
+      message: ERRORS_MESSAGE.NOT_FOUND,
+      error: new Error('Nenhum resultado retornado'),
+    }
+  }
+
+  return result;
+};
+
+const create = async () => {
+  const result = productsModel.create(id);
+
+  if (!result) {
+    return {
+      type: ERRORS_TYPE.NOT_FOUND,
+      message: ERRORS_MESSAGE.NOT_FOUND,
+      error: new Error('Nenhum resultado retornado'),
+    }
+  }
+
+  return result;
+};
+
+const update = async () => {
+  const result = productsModel.update(id);
+
+  if (!result) {
+    return {
+      type: ERRORS_TYPE.NOT_FOUND,
+      message: ERRORS_MESSAGE.NOT_FOUND,
+      error: new Error('Nenhum resultado retornado'),
+    }
+  }
+
+  return result;
+};
+
+const remove = async () => {
+  const result = productsModel.remove(id);
+
+  if (!result) {
+    return {
+      type: ERRORS_TYPE.NOT_FOUND,
+      message: ERRORS_MESSAGE.NOT_FOUND,
+      error: new Error('Nenhum resultado retornado'),
+    }
+  }
+
+  return result;
+};
+
+
+module.exports = {
+  findAll,
+  findById,
+  findBy,
+  create,
+  update,
+  remove,
+};
