3ca72ff3e020ce31ca13311fb8e14743d7f1c65f
gmaralan <gmartinialan@gmail.com>
2023-03-21 07:06:32 -0400

    feat: created sales router skeleton

--- src/routes/sales.router.js
+++ src/routes/sales.router.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const { salesController } = require('../controllers');
 
 const salesRouter = express.Router();
 
+salesRouter.get('/', salesController.findAll);
+salesRouter.get('/:id', salesController.findById);
+salesRouter.get('/search', salesController.findByQuery);
+salesRouter.post('/', salesController.create);
+salesRouter.put('/:id', salesController.update);
+salesRouter.delete('/:id', salesController.delete);
 
 module.exports = salesRouter;
